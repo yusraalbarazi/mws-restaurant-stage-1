67ed0b3dc6e2fe1ba22f02e4e93b4920dd27a3b3
ysraalbarazi <barazi.ysra@gmail.com>
2018-03-29 01:19:32 +0300

    step1

---
+++ js/swController.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('./sw.js')
+        .then(function(registeration) {
+            console.log("service worker registered", registeration);
+        }).catch(function(err) {
+            console.log("service worker failed to register", err);
+        })
+}
