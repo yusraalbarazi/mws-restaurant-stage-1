c6f9c0cc91b5cac9b3d8d38d509cc0fcf547c4e1
ysraalbarazi <barazi.ysra@gmail.com>
2018-03-29 01:23:43 +0300

    serviceWorker

--- js/swController.js
+++ js/swController.js
@@ -4,6 +4,6 @@ if ('serviceWorker' in navigator) {
         .then(function(registeration) {
             console.log("service worker registered", registeration);
         }).catch(function(err) {
-            console.log("service worker failed to register", err);
+            console.log("service worker failed to register s", err);
         })
 }
